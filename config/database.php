6e68242163eb6230185511e72cc095c5c6fde0ce
carlos727 <carlos72793@hotmail.com>
2016-05-09 19:03:07 -0500

    Convert to use Heroku PostgreSQL database

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
-
-	$url = parse_url(getenv("DATABASE_URL"));
-
-	$host = $url["host"];
-	$username = $url["user"];
-	$password = $url["pass"];
-	$database = substr($url["path"], 1);
 	/*
 	|--------------------------------------------------------------------------
 	| PDO Fetch Style
